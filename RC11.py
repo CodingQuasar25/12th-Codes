0e1b8adc86dd676a61b10c5edfc2dc7138c1aa8f
CodingQuasar25 <165765860+CodingQuasar25@users.noreply.github.com>
2024-06-21 16:34:22 +0530

    Update RC11.py

--- RC11.py
+++ RC11.py
@@ -14,12 +14,16 @@ def openname() :
 def stenword ():
     with open ('abc.txt','r') as f:
         c=f.readline()
+        ct=0
         while (c):
             c=c.split()
             for i in c:
                 if i[0].lower() in 'aeiou' and i[-1].lower() in 'aeiou':
                     print(i)
+                    ct+=1
             c=f.readline()
+        if ct==0:
+                print('None')
 def count():
     with open ('abc.txt','r') as f:
         c=0
@@ -27,8 +31,21 @@ def count():
         for i in s:
             c+=len(i.split())
         print('Total number of words in the file: ',c)
+def palin():
+    with open ('abc.txt','r') as f:
+        c=f.readline()
+        ct=0
+        while (c):
+            c=c.split()
+            for i in c:
+                if i.lower()==i[::-1].lower() and len(i)!=1:
+                    print(i)
+                    ct+=1
+            c=f.readline()
+        if ct==0:
+                print('None')
 while True :
-    n=int(input('Menu Driven Code:\n1.Create a File\n2.Display\n3.Display Count of Words\n4.Display words that start and end with a vowel\n5.Exit\nEnter Choice:'))
+    n=int(input('Menu Driven Code:\n1.Create a File\n2.Display\n3.Display Count of Words\n4.Display the words that are palindrome\n5.Display words that start and end with a vowel\n6.Exit\nEnter Choice:'))
     if n==1:
         name()
     elif n==2:
@@ -37,9 +54,14 @@ while True :
         count()
         print()
     elif n==4:
-        stenword()
+        print('The palindrome words are :')
+        palin()
         print()
     elif n==5:
+        print('The words that start and end with a vowel :')
+        stenword()
+        print()
+    elif n==6:
         break
     else:
         print('You have entered the wrong choice')
